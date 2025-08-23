9558f5ae42febb3c35d80a3a618b10bda541f5d3
westlakestore <animepplushies@gmail.com>
2025-08-22 21:28:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WestlakeLiquor/',
+  base: '/WestlakeLiquor/', // This must match your repo name
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
